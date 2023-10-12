c71774f15104acb196dbe39e00fb582aa7c90a7e
TechboyVerma <parikshitverma865@gmail.com>
2023-10-12 21:58:23 +0530

    final commit

---
+++ app/src/main/java/com/verma/retrofit_example/RequestPost.java
@@ -0,0 +1,29 @@
+package com.verma.retrofit_example;
+
+public class RequestPost {
+
+    public RequestPost(String name, String job) {
+        this.name = name;
+        this.job = job;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    String name,job;
+
+
+}
